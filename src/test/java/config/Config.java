7e1a44312693bf46a85b54393ea5dadf20eba938
preethi-samala244 <Preethi23244@gmail.com>
2026-03-02 09:27:52 +0530

    Create Config.java

---
+++ src/test/java/config/Config.java
@@ -0,0 +1,19 @@
+package config;
+
+public class Config {
+
+    public static final String BASE_URL =
+            "https://wmxrwq14uc.execute-api.us-east-1.amazonaws.com/Prod";
+
+    public static final String LOGIN_URL =
+            BASE_URL + "/Account/Login";
+
+    public static final String USERNAME =
+            "TestUser905";
+
+    public static final String PASSWORD =
+            "qrbK+-R)jn!P";
+
+    public static final String AUTH_TOKEN =
+            "Basic VGVzdFVzZXI5MDU6cXJiSystUilqbiFQ";
+}
